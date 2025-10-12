d391bd2b977c6d6160169dc5cf1b3e9fb8c1ed57
yongchane <202021113@sangmyung.kr>
2025-10-12 22:24:06 +0900

    #2 제품 상세 페이지 임의 ui 적용

--- src/app/settings/basic/layout.tsx
+++ src/app/settings/basic/layout.tsx
@@ -1,3 +1,3 @@
 export default function layout({ children }: { children: React.ReactNode }) {
-  return <main className="w-full h-full p-[48px]"> {children}</main>;
+  return <main className=""> {children}</main>;
 }
--- src/app/settings/components/NavMenu.tsx
+++ src/app/settings/components/NavMenu.tsx
@@ -10,7 +10,14 @@ const menu = [
 
 export default function NavMenu() {
   const pathname = usePathname();
-  const isSelected = (href: string) => pathname === href;
+
+  // 현재 경로가 특정 메뉴 항목과 일치하는지 확인
+  const isActive = (href: string) => {
+    if (!pathname) return false;
+    // 정확히 일치하거나, 하위 경로인 경우
+    return pathname === href || pathname.startsWith(href + "/");
+  };
+
   return (
     <nav className="pretendard w-full  flex justify-center items-center gap-[50px] ">
       {menu.map((item) => (
@@ -18,7 +25,7 @@ export default function NavMenu() {
           <Link href={item.href} className=" text-[16px]  font-medium ">
             <li
               className={`flex justify-center items-center w-[390px] h-[48px] border border-[#0089D1] rounded-[9999px] ${
-                isSelected(item.href)
+                isActive(item.href)
                   ? "bg-[#0089D1] text-white"
                   : "text-[#0089D1]"
               }`}
