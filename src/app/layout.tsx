067e9f9b8024257bcdecd35273fd1744010bb3e5
yongchane <202021113@sangmyung.kr>
2026-01-01 18:02:30 +0900

    카카오톡 openGraph 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,22 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "kitagawa 관리자 페이지",
   description: "kitagawa 홈페이지 및 제품 관리 페이지 입니다.",
+  openGraph: {
+    title: "Kitagawa 관리자 페이지",
+    description: "Kitagawa 홈페이지 및 제품 관리 페이지 입니다.",
+    url: "https://admin.kitagawa.co.kr",
+    siteName: "Kitagawa Admin",
+    locale: "ko_KR",
+    type: "website",
+    images: [
+      {
+        url: "https://storage.googleapis.com/kitagawa-cdn/banner/1763979224078-main1.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Kitagawa Admin",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
